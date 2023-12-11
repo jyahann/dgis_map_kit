6e565beea56f94b211134eda2efadcfe4db42a12
jyahann <raishi24e@gmail.com>
2023-12-11 17:49:30 +0600

    big refactor

---
+++ ios/Classes/DGisMapLayer.swift
@@ -0,0 +1,8 @@
+//
+//  DGisMapLayer.swift
+//  dgis_map_kit
+//
+//  Created by Rayimbek Takshibayev on 10.12.2023.
+//
+
+import Foundation
---
+++ ios/Classes/PolylinesUtils.swift
@@ -0,0 +1,8 @@
+//
+//  PolylinesUtils.swift
+//  dgis_map_kit
+//
+//  Created by Rayimbek Takshibayev on 10.12.2023.
+//
+
+import Foundation
